33fb5c3b3e53ae41436c001aac1b394dda0c1a53
Martinski <119833648+Martinski4GitHub@users.noreply.github.com>
2024-03-05 21:26:26 -0800

    Update CustomEMailFunctions.lib.sh
    Revert to previous version.

--- EMail/CustomEMailFunctions.lib.sh
+++ EMail/CustomEMailFunctions.lib.sh
@@ -7,7 +7,7 @@
 # email notifications using AMTM optional email config.
 #
 # Creation Date: 2020-Jun-11 [Martinski W.]
-# Last Modified: 2024-Mar-05 [Martinski W.]
+# Last Modified: 2024-Feb-18 [Martinski W.]
 ######################################################################
 
 if [ -z "${_LIB_CustomEMailFunctions_SHELL_:+xSETx}" ]
@@ -15,7 +15,7 @@ then _LIB_CustomEMailFunctions_SHELL_=0
 else return 0
 fi
 
-CEM_LIB_VERSION="0.9.16"
+CEM_LIB_VERSION="0.9.15"
 CEM_TXT_VERFILE="cemVersion.txt"
 
 CEM_LIB_SCRIPT_TAG="master"
@@ -58,9 +58,7 @@ amtmIsEMailConfigFileEnabled=false
 cemDateTimeFormat="%Y-%b-%d %a %I:%M:%S %p %Z"
 
 cemIsInteractive=false
-if echo "$cemScriptDirPath" | grep -qE "^[.]" || \
-   { [ -t 0 ] && ! tty | grep -qwi "not" ; }
-then cemIsInteractive=true ; fi
+[ -t 0 ] && ! tty | grep -qwi "NOT" && cemIsInteractive=true
 if ! "$cemIsInteractive" ; then cemIsVerboseMode=false ; fi
 
 #------------------------------------#
@@ -108,7 +106,7 @@ _CheckLibraryUpdates_CEM_()
    {
       if [ $# -eq 0 ] || [ -z "$1" ] ; then echo 0 ; return 1 ; fi
       local verNum  verStr
-
+      
       verStr="$(echo "$1" | sed "s/['\"]//g")"
       verNum="$(echo "$verStr" | awk -F '.' '{printf ("%d%02d%02d\n", $1,$2,$3);}')"
       verNum="$(echo "$verNum" | sed 's/^0*//')"
@@ -124,7 +122,7 @@ _CheckLibraryUpdates_CEM_()
    local libraryVerNum  dlCheckVerNum  dlCheckVerStr
    local showMsg  retCode
 
-   if [ $# -gt 1 ] && echo "$2" | grep -qE "^[-]?quiet$"
+   if [ $# -gt 1 ] && [ "$2" = "quiet" ]
    then showMsg=false ; else showMsg=true ; fi
 
    "$cemIsInteractive" && "$cemIsVerboseMode" && "$showMsg" && \
@@ -196,6 +194,7 @@ CheckEMailConfigFileFromAMTM_CEM_()
            printf "\nSome email configuration variables are not yet set up.\n"
        fi
        return 1
+   
    fi
 
    amtmIsEMailConfigFileEnabled=true
@@ -223,17 +222,11 @@ _CreateEMailContent_CEM_()
         emailBodyMsge="$(cat "$emailBodyFile")"
         rm -f "$emailBodyFile"
     fi
+    ! "$cemIsFormatHTML" && \
+    emailBodyMsge="$(echo "$emailBodyMsge" | sed 's/[<]b[>]//g ; s/[<]\/b[>]//g')"
 
-    [ $# -gt 2 ] && [ -n "$3" ] && emailBodyTitle="$3"
-
-    if "$cemIsFormatHTML"
-    then
-        ! echo "$emailBodyTitle" | grep -qE "^[<]h[1-5][>].*[<]/h[1-5][>]$" && \
-        emailBodyTitle="<h2>${emailBodyTitle}</h2>"
-    else
-        emailBodyMsge="$(echo "$emailBodyMsge" | sed 's/[<]b[>]//g ; s/[<]\/b[>]//g')"
-        emailBodyTitle="$(echo "$emailBodyMsge" | sed 's/[<]h[1-5][>]//g ; s/[<]\/h[1-5][>]//g')"
-    fi
+    if [ $# -gt 2 ] && [ -n "$3" ]
+    then emailBodyTitle="$3" ; fi
 
     if [ -n "$CC_NAME" ] && [ -n "$CC_ADDRESS" ]
     then
@@ -265,7 +258,7 @@ Content-Disposition: inline
 
 <!DOCTYPE html><html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head>
-<body>$emailBodyTitle
+<body><h2>${emailBodyTitle}</h2>
 <div style="color:black; font-family: sans-serif; font-size:130%;"><pre>
 EOF
     else
